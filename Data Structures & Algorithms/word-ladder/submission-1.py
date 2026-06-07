68620ad9f7e8154c002cbfb38a4c030c0a6522e7
tianyuli0126 <110426018+Lawrence2005@users.noreply.github.com>
2026-06-07 00:23:06 -0500

    Add: word-ladder - submission-1

---
+++ Data Structures & Algorithms/word-ladder/submission-1.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        self.adjList = {}
+        for word in wordList + [beginWord]:
+            self.adjList[word] = []
+            for other in wordList + [beginWord]:
+                if self.wordDist(word, other) != 1:
+                    continue
+                
+                self.adjList[word].append(other)
+        if endWord not in self.adjList:
+            return 0
+        
+        return self.helper(beginWord, endWord, set())
+    
+    def helper(self, start, end, visited):
+        q = deque([(start, 0)])
+        visited.add(start)
+        while q:
+            curr, currLen = q.popleft()
+            for neighbor in self.adjList[curr]:
+                if neighbor == end:
+                    return currLen + 2
+
+                if neighbor in visited:
+                    continue
+
+                visited.add(neighbor)
+                q.append((neighbor, currLen + 1))
+        return 0
+
+    def wordDist(self, w1, w2):
+        distance = 0
+        for i in range(len(w1)):
+            if w1[i] != w2[i]:
+                distance += 1
+        return distance
