eac31289180815624096aa5243ff12c2997b4a13
tianyuli0126 <110426018+Lawrence2005@users.noreply.github.com>
2026-06-07 01:11:45 -0500

    Add: network-delay-time - submission-1

---
+++ Data Structures & Algorithms/network-delay-time/submission-1.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        self.adjList = {node: [] for node in range(n + 1)}
+        for u, v, t in times:
+            self.adjList[u].append((v, t))
+
+        maxTime, shortest = float('-inf'), [float('inf') for node in range(n)]
+        shortest[k - 1] = 0
+        minHeap = [(0, k)]
+        while minHeap:
+            currT, curr = heapq.heappop(minHeap)
+            if currT > shortest[curr - 1]:
+                continue
+
+            maxTime = max(maxTime, currT)
+            for neighbor, t in self.adjList[curr]:
+                if currT + t >= shortest[neighbor - 1]:
+                    continue
+                
+                shortest[neighbor - 1] = currT + t
+                heapq.heappush(minHeap, (currT + t, neighbor))
+        
+        return maxTime if float('inf') not in shortest else -1
