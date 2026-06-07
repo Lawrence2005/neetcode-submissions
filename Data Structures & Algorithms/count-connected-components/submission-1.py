7045ed11c1d2945c4995e431a86c6b313c40e0c4
tianyuli0126 <110426018+Lawrence2005@users.noreply.github.com>
2026-06-06 23:25:13 -0500

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        self.init(n)
+        for e in edges:
+            self.union(e[0], e[1])
+        
+        count = 0
+        for node in range(n):
+            if self.find(node) == node:
+                count += 1
+        return count
+        
+    def init(self, n):
+        self.father = []
+        for i in range(n):
+            self.father.append(i)
+        return
+    
+    def find(self, node):
+        if self.father[node] != node:
+            self.father[node] = self.find(self.father[node])
+        return self.father[node]
+    
+    def union(self, n1, n2):
+        r_n1 = self.find(n1)
+        r_n2 = self.find(n2)
+        if r_n1 != r_n2:
+            self.father[r_n1] = r_n2
+    
+    def isSame(self, n1, n2):
+        r_n1 = self.find(n1)
+        r_n2 = self.find(n2)
+        return r_n1 == r_n2
