aba7dab4f98e7da37c557d77ccec2679a24e24ef
tianyuli0126 <110426018+Lawrence2005@users.noreply.github.com>
2026-06-09 02:28:53 -0500

    Add: swim-in-rising-water - submission-5

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    DIRECTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        maxLvl = grid[0][0]
+        minH = [(grid[0][0], (0, 0))]
+        visited = set()
+        while minH:
+            elev, currPos = heapq.heappop(minH)
+            maxLvl = max(maxLvl, elev)
+            if currPos == (len(grid) - 1, len(grid[0]) - 1):
+                return maxLvl
+            
+            visited.add(currPos)
+            for d in self.DIRECTIONS:
+                neighbor = (currPos[0] + d[0], currPos[1] + d[1])
+                if not (0 <= neighbor[0] < len(grid) and 0 <= neighbor[1] < len(grid[0])) or neighbor in visited:
+                    continue
+                
+                heapq.heappush(minH, (grid[neighbor[0]][neighbor[1]], neighbor))
+        
+        return -1
