c32c34b89659d051181b1ed524dd98cbd1b4766f
tianyuli0126 <110426018+Lawrence2005@users.noreply.github.com>
2026-06-09 05:06:50 -0500

    Add: reconstruct-flight-path - submission-11

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-11.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        tickets.sort()
+        self.adjList = {}
+        for t in tickets:
+            if t[0] not in self.adjList:
+                self.adjList[t[0]] = []
+            self.adjList[t[0]].append(t[1])
+        
+        self.path = ["JFK"]
+        self.helper(len(tickets), "JFK", set())
+        return self.path
+
+    def helper(self, numTickets, curr, used):
+        if len(self.path) == numTickets + 1:
+            return True
+
+        if curr not in self.adjList:
+            return False
+
+        for i in range(len(self.adjList[curr])):
+            if (curr, self.adjList[curr][i], i) in used:
+                continue
+
+            self.path.append(self.adjList[curr][i])
+            used.add((curr, self.adjList[curr][i], i))
+            if self.helper(numTickets, self.adjList[curr][i], used):
+                return True
+            
+            self.path.pop()
+            used.remove((curr, self.adjList[curr][i], i))
+        
+        return False
