de5b2f49a63de59ee570c5dfa56de2c96e6eb40b
tianyuli0126 <110426018+Lawrence2005@users.noreply.github.com>
2026-06-12 09:22:08 -0500

    Add: regular-expression-matching - submission-1

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        self.dp = {}
+
+        return self.helper(s, p, 0, 0)
+    
+    def helper(self, s, p, sIdx, pIdx):
+        if (sIdx, pIdx) in self.dp:
+            return self.dp[(sIdx, pIdx)]
+        
+        if sIdx >= len(s) and pIdx >= len(p):
+            return True
+        
+        if pIdx >= len(p):
+            return False
+        
+        match = sIdx < len(s) and (s[sIdx] == p[pIdx] or p[pIdx] == '.')
+        if pIdx + 1 < len(p) and p[pIdx + 1] == '*':
+            self.dp[(sIdx, pIdx)] = self.helper(s, p, sIdx, pIdx + 2) or (match and self.helper(s, p, sIdx + 1, pIdx))
+            return self.dp[(sIdx, pIdx)]
+        
+        if match:
+            self.dp[(sIdx, pIdx)] = self.helper(s, p, sIdx + 1, pIdx + 1)
+            return self.dp[(sIdx, pIdx)]
+        
+        self.dp[(sIdx, pIdx)] = False
+        return False
