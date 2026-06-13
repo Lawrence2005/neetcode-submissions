151e5594b6fbc0c4e6ed9fae926aa26098685726
tianyuli0126 <110426018+Lawrence2005@users.noreply.github.com>
2026-06-12 23:50:01 -0500

    Add: partition-labels - submission-1

---
+++ Data Structures & Algorithms/partition-labels/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        lastOccur = {}
+        for i in range(len(s)):
+            lastOccur[s[i]] = i
+        
+        result = []
+        l, r = 0, 0
+        for i in range(len(s)):
+            r = max(r, lastOccur[s[i]])
+            if i == r:
+                result.append(r - l + 1)
+                l = r + 1
+                r = l
+        return result
